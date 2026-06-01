e384696754636e54e243591c5e2a28756c695047
Ружена <NasyrovaRu22@yandex.ru>
2026-06-01 19:28:23 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,4 @@
-// Функция для поиска блока с максимальной суммой цифр
-function findMaxBlock(str) {
-  // Разбиваем строку по символу '0' и фильтруем пустые строки
-  const blocks = str.split('0').filter(block => block.length > 0);
-
-  let maxSum = -1;
-  let maxBlock = '';
-
-  // Проходим по каждому блоку
-  for (const block of blocks) {
-    // Считаем сумму цифр в блоке
-    const sum = block.split('').reduce((acc, digit) => acc + parseInt(digit), 0);
-
-    // Обновляем максимум
-    if (sum > maxSum) {
-      maxSum = sum;
-      maxBlock = block;
-    }
-  }
-
-  return [maxBlock, maxSum];
-}
-
-// Пример вызова функции (можно удалить или закомментировать)
-console.log(findMaxBlock('721024501111111090')); // ['245', 11]
-
-// Инициализация Vue.js приложения
-const { createApp } = Vue;
-
-createApp({
+const app = Vue.createApp({
   data() {
     return {
       colors: ['red', 'green', 'blue', 'yellow', 'purple']
@@ -35,8 +6,11 @@ createApp({
   },
   methods: {
     changeColor(index) {
-      const randomColors = ['red', 'green', 'blue', 'yellow', 'purple'];
-      this.colors[index] = randomColors[Math.floor(Math.random() * randomColors.length)];
+      // Меняем цвет на случайный из массива
+      const randomIndex = Math.floor(Math.random() * this.colors.length);
+      this.colors[index] = this.colors[randomIndex];
     }
   }
-}).mount('#app');
+});
+
+app.mount('#app');
